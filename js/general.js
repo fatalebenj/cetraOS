914bca0d630575a484b35706e9f048d72a945559
benjamin constant <fatalebenj@outlook.com>
2025-09-06 01:46:34 -0300

    Implement dark theme toggle functionality and update styles

--- js/general.js
+++ js/general.js
@@ -1,1 +1,8 @@
-c
+const toggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+    body.classList.toggle("darktheme");
+    document.getElementById("taskbar").classList.toggle("darktheme");
+    document.getElementById("theme-toggle").classList.toggle("darktheme");
+})
