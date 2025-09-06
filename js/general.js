f96fb736cc40bef8f8a2435912f5095599be3283
benjamin constant <fatalebenj@outlook.com>
2025-09-06 03:23:20 -0300

    Add trashbin shortcut and update theme toggle button icon

--- js/general.js
+++ js/general.js
@@ -1,8 +1,18 @@
 const toggleButton = document.getElementById("theme-toggle");
 const body = document.body;
+let darkMode = false;
 
 toggleButton.addEventListener("click", () => {
+    darkMode = !darkMode
     body.classList.toggle("darktheme");
     document.getElementById("taskbar").classList.toggle("darktheme");
-    document.getElementById("theme-toggle").classList.toggle("darktheme");
+    toggleButton.classList.toggle("darktheme");
+
+    if (darkMode == true){
+        toggleButton.innerHTML === '<i class="fa-light fa-moon"></i>';
+    }
+    else {
+        toggleButton.innerHTML === '<i class="fa-light fa-sun"></i>';
+    }
+
 })
