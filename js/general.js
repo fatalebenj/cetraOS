bb4b958b00e73c458efd252d44df3f8a0c511886
benjamin constant <fatalebenj@outlook.com>
2025-09-06 22:56:24 -0300

    cloud

--- js/general.js
+++ js/general.js
@@ -23,3 +23,17 @@ toggleButton.addEventListener('click', () => {
     localStorage.setItem('theme', 'dark');
   }
 });
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+  document.getElementById('clock').innerHTML =  h + ":" + m;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
