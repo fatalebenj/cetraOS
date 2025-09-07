d35c4b1ab265d8451873f2e78546d1e254362c29
benjamin constant <fatalebenj@outlook.com>
2025-09-06 21:26:24 -0300

    clou

--- js/general.js
+++ js/general.js
@@ -1,22 +1,25 @@
 const toggleButton = document.getElementById('theme-toggle');
 const taskbar = document.getElementById('taskbar');
+const toggleButtonIcon = document.createElement('span');
+toggleButtonIcon.id = 'theme-toggle-icon'; 
+toggleButton.appendChild(toggleButtonIcon);
 
 // Load saved theme from localStorage
 const savedTheme = localStorage.getItem('theme') || 'light';
 document.body.classList.add(savedTheme + '-mode');
-toggleButton.textContent = savedTheme === 'dark' ? '🌙' : '🌞';
+toggleButtonIcon.innerHTML = savedTheme === 'dark' ? '<i class="fa-solid fa-sun"></i>' : '<i class="fa-solid fa-moon"></i>';
 
 // Toggle theme on button click
 toggleButton.addEventListener('click', () => {
   if (document.body.classList.contains('dark-mode')) {
     taskbar.classList.replace('dark-mode', 'light-mode');
     document.body.classList.replace('dark-mode', 'light-mode');
-    toggleButton.textContent = '🌞';
+    toggleButtonIcon.innerHTML = '<i class="fa-solid fa-moon"></i>';
     localStorage.setItem('theme', 'light');
   } else {
     taskbar.classList.replace('light-mode', 'dark-mode');
     document.body.classList.replace('light-mode', 'dark-mode');
-    toggleButton.textContent = '🌙';
+    toggleButtonIcon.innerHTML = '<i class="fa-solid fa-sun"></i>';
     localStorage.setItem('theme', 'dark');
   }
 });
